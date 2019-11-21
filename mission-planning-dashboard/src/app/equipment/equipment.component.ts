03f731abf0633fbc03674927d8ec411fda355604
Robin Jun <robinjun@Robins-MacBook-Pro-2.local>
2019-11-20 18:48:41 -0600

    added studio requirements

---
+++ mission-planning-dashboard/src/app/equipment/equipment.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment',
+  templateUrl: './equipment.component.html',
+  styleUrls: ['./equipment.component.css']
+})
+export class EquipmentComponent implements OnInit {
+  equipment = [
+    'Habitat dome',
+    'Drones',
+    'Food containers',
+    'Oxygen tanks'
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ mission-planning-dashboard/src/app/experiments/experiments.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiments',
+  templateUrl: './experiments.component.html',
+  styleUrls: ['./experiments.component.css']
+})
+export class ExperimentsComponent implements OnInit {
+  experiments = [
+    'Mars soil sample', 
+    'Plant growth in habitat',
+    'Human bone density'
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
