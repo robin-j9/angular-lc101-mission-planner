71ea725d3d993d2d98ac805752e5c776c7091013
Robin Jun <robinjun@Robins-MacBook-Pro-2.local>
2019-12-14 23:23:45 -0600

    added addCrewMember function and click handler for candidates

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -25,5 +25,12 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
-
+  addCrewMember(candidate: object) {
+    if (this.crew.length < 3 && !this.crew.includes(candidate)) {
+      this.crew.push(candidate);
+    } else if (this.crew.includes(candidate)) {
+      let index = this.crew.indexOf(candidate);
+      this.crew.splice(index, 1);
+    }
+  }
 }
